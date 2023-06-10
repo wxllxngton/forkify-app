aa95dc1d54f0f967a954986cf5b5995f31bf099e
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-10 11:36:13 +0300

    Add files via upload

---
+++ src/views/resultsView.js
@@ -0,0 +1,16 @@
+import PreviewView from './previewView.js';
+import icons from 'url:../../img/icons.svg';
+
+class ResultsView extends PreviewView {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query! Please try again!';
+  _sortBtn = document.querySelector('.nav__btn--sort');
+
+  _addHandlerSort(handler) {
+    this._sortBtn.addEventListener('click', function (e) {
+      handler();
+    });
+  }
+}
+
+export default new ResultsView();
