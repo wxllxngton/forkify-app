3f1e43e436d7d3bdc312e9e6cd16b3a8593bea05
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-10 11:41:50 +0300

    Delete config.js

--- src/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 25;
-export const RES_PER_PAGE = 10;
-export const KEY = 'a7c13931-c4de-455f-8a99-36f941d57f5e';
-export const MODAL_CLOSE_SEC = 2.5;
